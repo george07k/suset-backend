093b226b89df058f310289f8babdcdbd2feabeaf
george07k <yornoel9@gmail.com>
2026-04-14 11:31:44 -0500

    Refactor index.js to improve structure and add root route

--- index.js
+++ index.js
@@ -42,4 +42,8 @@ app.get("/servicios", async (req, res) => {
   res.json(result.rows);
 });
 
+app.get("/", (req, res) => {
+  res.send("Backend funcionando");
+});
+
 app.listen(3000, () => console.log("Servidor activo"));
